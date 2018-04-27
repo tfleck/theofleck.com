fa4477993ea64e491bf218a767c9fb6543c78651
Theo Fleck <emailtfleck@gmail.com>
2018-04-27 14:10:51 -0400

    Rewrite in Bootstrap v4
    Ground-up rewrite without template, optimized for speed

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Populate youtube video after DOM loaded
+$(document).ready(function(){
+    $('iframe#ytvideo').attr('src', 'https://www.youtube-nocookie.com/embed/L9VBpbnXhWk?rel=0');
+});
+//Shrink navbar on scroll
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+// Add smooth scrolling on all links inside the navbar
+$("nav a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
